701f536855f8cbbffaf647817991b4069d4a235d
JaySong <qjay.dev@gmail.com>
2015-08-28 18:27:32 +0800

    添加匹配工具类

---
+++ android-utils-library/src/main/java/com/qjay/android_utils/MatcherUtil.java
@@ -0,0 +1,12 @@
+package com.qjay.android_utils;
+
+/**
+ * /**
+ * 匹配工具类
+ * Created by Q.Jay on 2015/8/28 0028.
+ */
+public final class MatcherUtil {
+    private MatcherUtil() {
+        throw new UnsupportedOperationException("不支持实例化");
+    }
+}
