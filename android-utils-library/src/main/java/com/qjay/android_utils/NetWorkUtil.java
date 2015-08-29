706030a60f257cf03bf236793e16bf17509d9c15
JaySeng <qjay.dev@gmail.com>
2015-08-29 20:08:33 +0800

    网络工作工具类

---
+++ android-utils-library/src/main/java/com/qjay/android_utils/NetWorkUtil.java
@@ -0,0 +1,134 @@
+package com.qjay.android_utils;
+
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.telephony.TelephonyManager;
+import android.text.TextUtils;
+
+/**
+ * NetWork Utils
+ * <ul>
+ * <strong>Attentions</strong>
+ * <li>You should add <strong>android.permission.ACCESS_NETWORK_STATE</strong> in manifest, to get network status.</li>
+ * </ul>
+ * 
+ * @author <a href="http://www.trinea.cn" target="_blank">Trinea</a> 2014-11-03
+ */
+public class NetWorkUtil {
+
+    public static final String NETWORK_TYPE_WIFI       = "wifi";
+    public static final String NETWORK_TYPE_3G         = "eg";
+    public static final String NETWORK_TYPE_2G         = "2g";
+    public static final String NETWORK_TYPE_WAP        = "wap";
+    public static final String NETWORK_TYPE_UNKNOWN    = "unknown";
+    public static final String NETWORK_TYPE_DISCONNECT = "disconnect";
+
+    /**
+     * 判断当前是否有网络
+     * @param context 上下文
+     * @return 返回是否有网络
+     */
+    private static boolean isHasNet(Context context){
+        ConnectivityManager manager = (ConnectivityManager)context.getSystemService(Context.CONNECTIVITY_SERVICE);
+        if(null == manager){
+            return false;
+        }
+        NetworkInfo networkInfo = manager.getActiveNetworkInfo();
+        if(null == networkInfo){
+            return false;
+        }
+        return networkInfo.isConnected();
+    }
+    /**
+     * Get network type
+     * 获取当前网络类型
+     * @param context 上下文
+     * @return 当前网络类型
+     */
+    public static int getNetworkType(Context context) {
+        ConnectivityManager connectivityManager = (ConnectivityManager)context
+                .getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo networkInfo = connectivityManager == null ? null : connectivityManager.getActiveNetworkInfo();
+        return networkInfo == null ? -1 : networkInfo.getType();
+    }
+
+    /**
+     * Get network type name
+     * 获取当前网络类型名称
+     * @param context 上下文
+     * @return 当前网络类型名称
+     */
+    public static String getNetworkTypeName(Context context) {
+        ConnectivityManager manager = (ConnectivityManager)context.getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo networkInfo;
+        String type = NETWORK_TYPE_DISCONNECT;
+        if (manager == null || (networkInfo = manager.getActiveNetworkInfo()) == null) {
+            return type;
+        }
+
+        if (networkInfo.isConnected()) {
+            String typeName = networkInfo.getTypeName();
+            if ("WIFI".equalsIgnoreCase(typeName)) {
+                type = NETWORK_TYPE_WIFI;
+            } else if ("MOBILE".equalsIgnoreCase(typeName)) {
+                String proxyHost = android.net.Proxy.getDefaultHost();
+                type = TextUtils.isEmpty(proxyHost) ? (isFastMobileNetwork(context) ? NETWORK_TYPE_3G : NETWORK_TYPE_2G)
+                        : NETWORK_TYPE_WAP;
+            } else {
+                type = NETWORK_TYPE_UNKNOWN;
+            }
+        }
+        return type;
+    }
+
+    /**
+     * Whether is fast mobile network
+     * 是否是快速移动网络
+     * @param context 上下文
+     * @return 是否是快速移动网络
+     */
+    private static boolean isFastMobileNetwork(Context context) {
+        TelephonyManager telephonyManager = (TelephonyManager)context.getSystemService(Context.TELEPHONY_SERVICE);
+        if (telephonyManager == null) {
+            return false;
+        }
+
+        switch (telephonyManager.getNetworkType()) {
+            case TelephonyManager.NETWORK_TYPE_1xRTT:
+                return false;
+            case TelephonyManager.NETWORK_TYPE_CDMA:
+                return false;
+            case TelephonyManager.NETWORK_TYPE_EDGE:
+                return false;
+            case TelephonyManager.NETWORK_TYPE_EVDO_0:
+                return true;
+            case TelephonyManager.NETWORK_TYPE_EVDO_A:
+                return true;
+            case TelephonyManager.NETWORK_TYPE_GPRS:
+                return false;
+            case TelephonyManager.NETWORK_TYPE_HSDPA:
+                return true;
+            case TelephonyManager.NETWORK_TYPE_HSPA:
+                return true;
+            case TelephonyManager.NETWORK_TYPE_HSUPA:
+                return true;
+            case TelephonyManager.NETWORK_TYPE_UMTS:
+                return true;
+            case TelephonyManager.NETWORK_TYPE_EHRPD:
+                return true;
+            case TelephonyManager.NETWORK_TYPE_EVDO_B:
+                return true;
+            case TelephonyManager.NETWORK_TYPE_HSPAP:
+                return true;
+            case TelephonyManager.NETWORK_TYPE_IDEN:
+                return false;
+            case TelephonyManager.NETWORK_TYPE_LTE:
+                return true;
+            case TelephonyManager.NETWORK_TYPE_UNKNOWN:
+                return false;
+            default:
+                return false;
+        }
+    }
+}
